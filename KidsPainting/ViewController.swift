77ef0c4946529cfc7e8116e3d0e1d92286d08794
Amir <hashemi.eng1985@gmail.com>
2017-07-11 15:59:40 -0400

    Initial Commit

---
+++ KidsPainting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidsPainting
+//
+//  Created by seyedamirhossein hashemi on 2017-07-11.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
