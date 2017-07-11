a0a09bfa769af993328d88355d8eaa0cb1e97e70
FlorianDenu <florrian.denu@gmail.com>
2017-07-11 16:09:34 -0400

    Florian first commit to test

---
+++ KidsPainting/CredentialsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CredentialsViewController.swift
+//  KidsPainting
+//
+//  Created by emily on 2017-07-11.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import UIKit
+
+class CredentialsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
