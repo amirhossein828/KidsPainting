b175cf2d758d448db0935ce7bbf9c16e0b20d853
new_user_name <new@email.com>
2017-07-12 19:15:12 -0400

    login is done

--- KidsPainting/LoginViewController.swift
+++ KidsPainting/LoginViewController.swift
@@ -1,17 +1,21 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  KidsPainting
 //
-//  Created by seyedamirhossein hashemi on 2017-07-11.
+//  Created by seyedamirhossein hashemi on 2017-07-12.
 //  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailLoginField: UITextField!
 
+    @IBOutlet weak var passwordLoginField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
     }
 
@@ -19,7 +23,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         
     }
+    
 
-
+    @IBAction func loginBtn(_ sender: UIButton) {
+        // check email and password are not empty
+        guard emailLoginField.text != "", passwordLoginField.text != "" else {return}
+        // When a user signs in to your app, pass the user's email address and password to this method
+        Auth.auth().signIn(withEmail: emailLoginField.text!, password: passwordLoginField.text!, completion: { (user, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            // go to mai page
+            if let user = user {
+                let vc = UIStoryboard(name: "MainPage", bundle: nil).instantiateViewController(withIdentifier: "mainPage")
+                
+                self.present(vc, animated: true, completion: nil)
+            }
+        })
+        
+        
+    }
 }
+  
+
 
