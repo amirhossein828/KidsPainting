b7cd5a7e44f78ba7b05ad4ed87079b5f36276821
Masoud Bozorgi <masoud_b@yahoo.com>
2017-07-19 10:33:32 -0400

    Code cleaning

--- KidsPainting/LoginViewController.swift
+++ KidsPainting/LoginViewController.swift
@@ -10,35 +10,40 @@ import UIKit
 import Firebase
 
 class LoginViewController: UIViewController {
+    
     @IBOutlet weak var emailLoginField: UITextField!
-
     @IBOutlet weak var passwordLoginField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
     }
     
 
     @IBAction func loginBtn(_ sender: UIButton) {
-        // check email and password are not empty
+        
+        // Check email and password are not empty
         guard emailLoginField.text != "", passwordLoginField.text != "" else {return}
+        
         // When a user signs in to your app, pass the user's email address and password to this method
-        Auth.auth().signIn(withEmail: emailLoginField.text!, password: passwordLoginField.text!, completion: { (user, error) in
+        Auth.auth().signIn(withEmail: emailLoginField.text!,
+                           password: passwordLoginField.text!,
+                           completion: { (user, error) in
             
             if let error = error {
                 print(error.localizedDescription)
             }
             
-            // go to mai page
+            // Go to main page in other story board
             if let user = user {
-                let vc = UIStoryboard(name: "MainPage", bundle: nil).instantiateViewController(withIdentifier: "navigationPage")
                 
+                let vc = UIStoryboard(name: "MainPage", bundle: nil).instantiateViewController(withIdentifier: "navigationPage")
+                // Show new storyboard first controller
                 self.present(vc, animated: true, completion: nil)
             }
         })
--- KidsPainting/MainViewController.swift
+++ KidsPainting/MainViewController.swift
@@ -11,90 +11,121 @@ import Firebase
 
 class MainViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
 
+    //MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
-    var ref: DatabaseReference!
+    
+    //MARK: - Properties
     var items = [Item]()
-    var resultData : NSData = NSData()
+    var ref: DatabaseReference!
+
+    
+    
+    //var resultData : NSData = NSData()  //NSData is not used
     fileprivate var _refHandle: DatabaseHandle!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        // Already set by storyboard
+        //self.tableView.delegate = self
+        //self.tableView.dataSource = self
     }
-    override func viewWillAppear(_ animated: Bool) {
-        ref = Database.database().reference()
-        
-        ref.child("posts").observeSingleEvent(of: .value) { (dataSnap : DataSnapshot) in
-            let value = dataSnap.value as? NSDictionary
-            print(value)
-            let allAnswer = value?.allValues as? [[String:Any]]
-            guard let allAnswerOfPosts = allAnswer else {
-                return
-            }
-            for answer in allAnswerOfPosts {
-                
-                let item = Item()
-                item.author = answer["author"] as! String
-                
-                item.pathToImage = answer["pathToImage"] as! String
-                item.nameOfArticle = answer["nameOfArticle"] as! String
-                item.price = Double(answer["price"] as! String)
-                self.items.append(item)
-               
-                
-                
-            }
-            
-                
-                self.tableView.reloadData()
-                
-            
-           
-        }
-        ref.removeAllObservers()
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-    
+    // MARK: ------------------------------------------------- TableView initialization methods
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Return number of items in item array, if it does not exist return 0
+        return items.count ?? 0
+    }
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MainTableViewCell
         
-
        
-        
-        cell.fullNameField.text = self.items[indexPath.row].author
+        cell.fullNameField.text     = self.items[indexPath.row].author
         cell.nameOfArticleCell.text = self.items[indexPath.row].nameOfArticle
-        cell.priceCell.text = String(self.items[indexPath.row].price)
-      
-        cell.imageViewCell.downloadImage(from: items[indexPath.row].pathToImage)
+        cell.priceCell.text         = String(self.items[indexPath.row].price)
         
+        
+        // downloadImage method is implemented in an extention
+        cell.imageViewCell.downloadImage(from: items[indexPath.row].pathToImage)
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count ?? 0
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        items.removeAll()
-    }
     
+    // MARK: Fix cell height to fix appearance cell problem when height
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
-        return 100.0;//Choose your custom row height
+        return 100.0; //Choose your custom row height
+    }
+    
+    //-----------------------------------------------------------------------------------
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Fetch list of items from firebase backend
+        ref = Database.database().reference()
+        
+        // Access "posts" object in backend if it does not exist
+        ref.child("posts").observeSingleEvent(of: .value)
+        { (dataSnap : DataSnapshot) in
+            
+            let value = dataSnap.value as? NSDictionary
+            print(value as Any)
+            
+            let allAnswer = value?.allValues as? [[String:Any]]
+            
+            guard let allAnswerOfPosts = allAnswer
+                else { return }
+            
+            
+            // To prevent duplication item array can be erased here or in viewDidDisappear
+//            items.removeAll()
+            
+            for answer in allAnswerOfPosts
+            {
+                //------------------------------------------- To remove current user items from list of items
+//                if let uid = answer["uid"] as? String{
+//                    if uid != Auth.auth().currentUser!.uid{
+//                        // TODO: Create Item object to be added in item array
+//                    }
+//                }
+                //-------------------------------------------------------------------------------------------
+                
+                let item = Item()
+                
+                item.author = answer["author"] as! String
+                item.pathToImage = answer["pathToImage"] as! String
+                item.nameOfArticle = answer["nameOfArticle"] as! String
+                item.price = Double(answer["price"] as! String)
+                
+                self.items.append(item)
+            }
+            self.tableView.reloadData()
+        }
+        // To prevent crashing app if we leave current view controller which is under observation
+        ref.removeAllObservers()
     }
 
+    override func viewDidDisappear(_ animated: Bool) { items.removeAll() }
+    
+    
 }
 
 
@@ -104,8 +135,8 @@ extension UIImageView {
     func downloadImage(from imgURL: String!)  {
         
         let url = URLRequest(url: URL(string: imgURL)!)
-        let task = URLSession.shared.dataTask(with: url) {
-            (data, response, error) in
+        let task = URLSession.shared.dataTask(with: url)
+        { (data, response, error) in
             
             if error != nil {
                 print(error!)
@@ -115,11 +146,8 @@ extension UIImageView {
             DispatchQueue.main.async {
                 self.image = UIImage(data: data!)
             }
-            
         }
-        
         task.resume()
-        
     }
 }
 
