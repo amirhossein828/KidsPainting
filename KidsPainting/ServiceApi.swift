71de9ba7f0d57e2e61d91b482e1d48c44816d646
Amir <hashemi.eng1985@gmail.com>
2017-07-19 23:27:39 -0400

    I changed getting info from firebase, I created two new classes for that. serviceApi and ItemServiceApi

---
+++ KidsPainting/ServiceApi.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceApi.swift
+//  KidsPainting
+//
+//  Created by seyedamirhossein hashemi on 2017-07-19.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import Foundation
+// Protocol for API implementaion
+protocol ServiceApi {
+    func getAllItemsFrimFireBaseDataBase(complition : @escaping ([Item]) -> Void)
+}
