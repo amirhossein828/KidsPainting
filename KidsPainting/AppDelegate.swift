40f088f833ac42f60a6066ecf185681bc01a1e09
FlorianDenu <florian.denu@gmail.com>
2017-07-28 17:36:54 -0400

    Add google place, modify the first view that will be display regarding the user status (login/logout), add modification of user informations, modification of the user management layout.

--- KidsPainting/AppDelegate.swift
+++ KidsPainting/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate{
@@ -17,7 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        GMSPlacesClient.provideAPIKey("AIzaSyAwycclhIKgID76swMuC4zdujGcyLqcaWM")
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        //Choose which view should be display first
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let loginStoryboard = UIStoryboard(name: "Login", bundle: nil)
+        let mainStoryboard = UIStoryboard(name: "MainPage", bundle: nil)
+        if Auth.auth().currentUser?.email != nil{
+            self.window?.rootViewController = mainStoryboard.instantiateViewController(withIdentifier: "mainPage")
+            self.window?.makeKeyAndVisible()
+        }else{
+            self.window?.rootViewController = loginStoryboard.instantiateViewController(withIdentifier: "credentialsViewController")
+            self.window?.makeKeyAndVisible()
+        }
         return true
     }
     
