92fe4e106e814f50aa61d640b70c811bdfc5645a
new_user_name <new@email.com>
2017-08-02 12:12:20 -0400

    write some comment for CameraViewController.

--- KidsPainting/CameraViewController.swift
+++ KidsPainting/CameraViewController.swift
@@ -9,27 +9,50 @@
 import UIKit
 
 class CameraViewController: UIViewController {
+    var imagePicker: UIImagePickerController!
+    
+    var imageCamera : UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // put image taken from camera inside upload view controller
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "goFromCameraToUpload" {
+            
+            if let vc = segue.destination as? UploadItemViewController {
+                vc.newImage = self.imageCamera
+            }
+        }
     }
-    */
 
+}
+
+//This protocol is used to take a picture, when the photo is validate by the
+//user, the function imagePickerController is called
+extension CameraViewController : UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        //If the image exists extract it and store in a constant
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            self.imageCamera = image
+            //go to uploadviewcontroller
+            performSegue(withIdentifier: "goFromCameraToUpload", sender: self)
+            //profilePictureButton.setImage(image, for: .normal)
+        }
+    }
+}
+
+extension CameraViewController: UINavigationControllerDelegate{
+    
 }
