9cc642536c9839b2e90db8b9fd14dcb06a1c5e93
Amir <hashemi.eng1985@gmail.com>
2017-12-30 22:48:15 -0500

    Add profile Photo in cells.

--- KidsPainting/MainTableViewCell.swift
+++ KidsPainting/MainTableViewCell.swift
@@ -11,9 +11,12 @@ import UIKit
 class MainTableViewCell: UITableViewCell  {
     
     @IBOutlet weak var imageViewCell: UIImageView!
-    
-    
     @IBOutlet weak var fullNameField: UILabel!
     @IBOutlet weak var nameOfArticleCell: UILabel!
     @IBOutlet weak var priceCell: UILabel!
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    override func awakeFromNib() {
+        self.profilePhoto.layer.cornerRadius = self.profilePhoto.frame.size.width / 2
+    }
 }
